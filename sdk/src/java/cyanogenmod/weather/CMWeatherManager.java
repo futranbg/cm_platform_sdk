13df2ba9e495c9e680076a07ba21a944bb761c4b
Luis Vidal <lvidal@cyngn.com>
2016-06-16 17:25:29 -0700

    cmsdk: Prevent NPE in CMWeatherManager
    Verify that we have an instance of to the weather manager service
    before we try to register/unregister a listener or query the
    current weather provider.
    
    Change-Id: Iec09073615f85626c6f64e4a55758aae52231ca8

--- sdk/src/java/cyanogenmod/weather/CMWeatherManager.java
+++ sdk/src/java/cyanogenmod/weather/CMWeatherManager.java
@@ -245,6 +245,8 @@ public class CMWeatherManager {
      */
     public void registerWeatherServiceProviderChangeListener(
             @NonNull WeatherServiceProviderChangeListener listener) {
+        if (sWeatherManagerService == null) return;
+
         synchronized (mProviderChangedListeners) {
             if (mProviderChangedListeners.contains(listener)) {
                 throw new IllegalArgumentException("Listener already registered");
@@ -266,6 +268,8 @@ public class CMWeatherManager {
      */
     public void unregisterWeatherServiceProviderChangeListener(
             @NonNull WeatherServiceProviderChangeListener listener) {
+        if (sWeatherManagerService == null) return;
+
         synchronized (mProviderChangedListeners) {
             if (!mProviderChangedListeners.contains(listener)) {
                 throw new IllegalArgumentException("Listener was never registered");
@@ -286,6 +290,8 @@ public class CMWeatherManager {
      * @return the service's label
      */
     public String getActiveWeatherServiceProviderLabel() {
+        if (sWeatherManagerService == null) return null;
+
         try {
             return sWeatherManagerService.getActiveWeatherServiceProviderLabel();
         } catch(RemoteException e){
